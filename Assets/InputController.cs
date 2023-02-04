a7ae127f28a5011368e3e17320d88b9aafc759f5
JonathanUrosa <51730896+JonathanUrosa@users.noreply.github.com>
2023-02-04 07:22:24 -0400

    add colores para carpetas, asi trabajamos mejor

---
+++ Assets/InputController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private Camera _camera;
+    public Camera Camera => _camera;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+        }    
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public void InvokeMovement(Vector3 position)
+    {
+
+    }
+
+    private void Move()
+    {
+
+    }
+
+    private void Rotate()
+    {
+
+    }
+}
